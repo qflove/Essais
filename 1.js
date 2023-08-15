e921c419d6cabb9c16953f9d4df0f3208d3a2e72
qflove <89767463+qflove@users.noreply.github.com>
2023-08-15 17:12:43 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,14 +1,9 @@
-/*
-
-NeckGo：https://apps.apple.com/app/id1660505668
-
 [rewrite_local]
 ^https?:\/\/neckgo\.wedea\.cn\/neok\/api\/api\/getLoginInfo url script-response-body https://raw.githubusercontent.com/qflove/Essais/main/1.js
 
 [MITM]
 hostname = neckgo.wedea.cn
 
-*/
 var guding = JSON.parse($response.body);
 guding.data.isVip = true;
 $done({ body: JSON.stringify(guding) });
